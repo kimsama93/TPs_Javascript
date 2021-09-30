f7eb6bf820e82baf6384f03a844b379acff55a71
Salma Histane <salma.histy@gmail.com>
2021-09-30 17:07:14 +0200

    add training js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-alert("bonjour a tous");
+// one line comment
 
-let firstname = "Jean";
+/*
+mutline comment
+*/
+
+//Déclaration de variables
+let firstName = "Jean";
 console.log(firstName);
 firstName = "Jonathan";
 console.log(firstName);
@@ -8,11 +13,45 @@ firstName = "Lydie";
 console.log(firstName);
 
 //Typage dynamique
-
 let age = 67;
 age = "89";
 
-//Constantes
-
+//Constantes : Ces valeurs ne peuvent pas être modifiées
 const accelerationPesanteur = 9.8;
 const pays = "France";
+
+//Type de données
+//String
+const lastName = "Jonathan";
+const firstname = "Jean";
+const fullName = lastName + " " + firstname;
+const fullName2 = `${lastName} ${firstName}`;
+
+//Number
+const age2 = 67;
+const ageDeMonPere = 12;
+const ageDeMaMere = 9;
+const sommeDesAges = ageDeMonPere + ageDeMaMere;
+
+let nombre = 45;
+let nombre2 = 26;
+let somme = nombre + nombre2;
+
+//Boolean
+const isMarried = true;
+
+// Prompter l'utilisateur
+const prenom = prompt("Quel est votre prénom ?");
+const nomDeFamille = prompt("Quel est votre nom de famille ?");
+const monAge = prompt("Quel est votre age ?");
+
+//Vous êtes Jean Blabla, et vous avez 45 ans
+alert(
+  "Vous etes " +
+    prenom +
+    " " +
+    nomDeFamille +
+    ", et vous  avez " +
+    monAge +
+    " ans"
+);
