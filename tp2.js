eb8d0816a1599ecc4bdb4042f37551344167e40d
Salma Histane <salma.histy@gmail.com>
2021-09-30 16:53:37 +0200

    add tp2 last

---
+++ tp2.js
@@ -0,0 +1,7 @@
+// Calcul de la vitesse en m/s
+const distance_parcours = prompt("Veuillez entrer la distance (m)");
+const temps = prompt("Veuillez entrer le temps (s)");
+
+const vitesse = distance_parcours / temps;
+
+alert("La vitesse est " + vitesse + " m/s");
