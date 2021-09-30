fdcea4feda10418c22e05a09ffe09ad7fbeb3c03
Salma Histane <salma.histy@gmail.com>
2021-09-30 16:53:03 +0200

    add tp1 last

--- tp1.js
+++ tp1.js
@@ -1,11 +1,8 @@
-let a1 = 45;
-let a2 = 26;
+alert("Bienvenue sur la page de formulaire");
+
+let a1 = parseInt(prompt("Veuillez entrer le premier nombre"));
+let a2 = parseInt(prompt("Veuillez entrer le deuxieme nombre"));
 
 const somme = a1 + a2;
 
-console.log("Bienvenue sur la page de formulaire");
-
-console.log("la premiere valeur est " + a1);
-console.log("la premiere valeur est " + a2);
-
-console.log("le résultat est " + somme);
+alert("Le resultat est " + somme);
