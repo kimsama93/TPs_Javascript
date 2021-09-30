cb78f405d38d44576f957b1b109ec8ec6624854a
Salma Histane <salma.histy@gmail.com>
2021-09-30 16:24:15 +0200

    add tp1

---
+++ tp1.js
@@ -0,0 +1,11 @@
+let a1 = 45;
+let a2 = 26;
+
+const somme = a1 + a2;
+
+console.log("Bienvenue sur la page de formulaire");
+
+console.log("la premiere valeur est " + a1);
+console.log("la premiere valeur est " + a2);
+
+console.log("le résultat est " + somme);
