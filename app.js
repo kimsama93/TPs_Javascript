5d0a926343110a2c1c1df6d25013ff1d0efb9a7f
Salma Histane <salma.histy@gmail.com>
2021-09-30 14:48:14 +0200

    add variables definition+const

---
+++ app.js
@@ -0,0 +1,18 @@
+alert("bonjour a tous");
+
+let firstname = "Jean";
+console.log(firstName);
+firstName = "Jonathan";
+console.log(firstName);
+firstName = "Lydie";
+console.log(firstName);
+
+//Typage dynamique
+
+let age = 67;
+age = "89";
+
+//Constantes
+
+const accelerationPesanteur = 9.8;
+const pays = "France";
