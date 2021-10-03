9a202e792c2f1d5557b3f7355f714ea836243a3f
Salma Histane <salma.histy@gmail.com>
2021-10-03 23:42:43 +0200

    add tp17

---
+++ tp17.js
@@ -0,0 +1,27 @@
+const tailleMatrice = parseInt(
+  prompt("Veuillez entrer la taille de la matrice")
+);
+
+const matrice = [];
+let somme = 0;
+let somme1 = 0;
+let somme2 = 0;
+
+for (let i = 0; i < tailleMatrice; i++) {
+  matrice[i] = [];
+}
+
+for (let i = 0; i < tailleMatrice; i++) {
+  for (let j = 0; j < tailleMatrice; j++) {
+    matrice[i][j] = parseInt(prompt("saisez les nombres de la matrice"));
+  }
+}
+
+for (let i = 0; i < tailleMatrice; i++) {
+  somme1 += matrice[i][i];
+  somme2 += matrice[i][matrice.length - i - 1];
+}
+
+somme = somme1 + somme2;
+console.log("La matrice est matrice = [" + matrice + "]");
+console.log("sommme de la diagonale est : " + somme);
