c6f3b3edc3d0cb08a985fcef9a91522c3807acdd
Salma Histane <salma.histy@gmail.com>
2021-10-04 11:19:17 +0200

    add tp7

--- tp7.js
+++ tp7.js
@@ -4,7 +4,7 @@ while (nombre < 10 || nombre > 20) {
   let nombre = prompt("Veuillez saisir un nombre entre 10 et 20");
   if (nombre > 20) {
     alert("plus petit ! ");
-  } else {
+  } else if (nombre < 10) {
     alert("plus grand ! ");
-  }
+  } else alert("Vous avez réussi !  ");
 }
